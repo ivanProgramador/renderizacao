5e347b79106d3da8076639e79fc3bfbf5c227991
Ivan Rodrigues <ivan-guita@hotmail.com>
2024-10-16 14:36:55 -0300

    fim das aulas de redenderização estática

--- pages/estatico2.jsx
+++ pages/estatico2.jsx
@@ -1,5 +1,9 @@
 
-
+/*
+No modo desenvolvimento ele muda porque ele vai recompilar para testar 
+No modo pordução(Build) ele vai manter o valor fixo com base no ultimo
+valor aleatório gerado
+*/
 export function getStaticProps(){
     return  {
         props: {
--- pages/estatico4.tsx
+++ pages/estatico4.tsx
@@ -1,24 +1,41 @@
 import React from "react";
 
+/*
+Nesse exemplo de renderização estática 
+existe uma função qu traz os dados da api
+e compila a aplicação com valores fixos. 
+
+*/
 
 export async function getStaticProps(){
 
    const resp = await fetch("http://localhost:3000/api/produtos");
+   const produtos = await resp.json();
 
      return{
         props:{
+            produtos
              
         }
      }
 }
 
-export default function Estatico4(){
+export default function Estatico4(props){
+
+   function renderizarProdutos(){
+        return props.produtos.map(produto=>{
+           return <li key={produto.id}>{produto.nome} tem preço de {produto.preco}</li>
+        })
+   }
+
+
+
      return(
         <div>
-
             <h2>Estatico#04</h2>
-            
-            
+            <ul>
+               {renderizarProdutos()}
+            </ul>
         </div>
      )
 }
